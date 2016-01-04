07031023ac942d0b704384fc46671f0f6033b4dd
JaeHan-Kim <hgal8877@gmail.com>
2016-01-04 14:14:44 +0900

    cycle.js수정

--- carmanager/src/main/webapp/js/cycle.js
+++ carmanager/src/main/webapp/js/cycle.js
@@ -6,14 +6,8 @@ $(document).ready(function() {
                 '../img/image002.jpg',
                 '../img/image020.jpeg'
                 ],
-<<<<<<< HEAD
-                fadeSpeed: 1500, //2000
-                duration: 4500,  //5000
-=======
-       
                 fadeSpeed: 4000, //2000
                 duration: 6000,  //5000
->>>>>>> e946281465d712cee10f4b1d5bea1f1e7f03b1de
                 backgroundSize: SCALING_MODE_COVER
   });
 });
