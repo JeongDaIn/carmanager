39e439d6ee58a8ad0b9fe733d3a0ceb7587d1a2d
taewoong-yun <inpotin03@gmail.com>
2016-01-06 19:58:31 +0900

    garage 작업중

--- carmanager/src/main/java/cms/domain/AjaxResult.java
+++ carmanager/src/main/java/cms/domain/AjaxResult.java
@@ -4,22 +4,32 @@ public class AjaxResult {
   protected String status;
   protected Object data;
   
-  public AjaxResult() {}
+  public AjaxResult() {
 
+  }
+  public AjaxResult(String status, Object data) {
+    this.status = status;
+    this.data = data;
+  }
+  
+  @Override
+  public String toString() {
+    return "AjaxResult [status=" + status + ", data=" + data + "]";
+  }
+  
   public String getStatus() {
     return status;
   }
-
-  public void setStatus(String status) {
+  public AjaxResult setStatus(String status) {
     this.status = status;
+    return this;
   }
-
   public Object getData() {
     return data;
   }
-
-  public void setData(Object data) {
+  public AjaxResult setData(Object data) {
     this.data = data;
+    return this;
   }
   
 }
