7272feadc95ab40568d7964bdcb402d8f14f83df
Song <ho4319@naver.com>
2016-01-06 20:12:48 +0900

    dispatch-servlet.xml 수정 : viewResolvers를 html로 받게 수정

--- carmanager/src/main/java/cms/controller/AuthController.java
+++ carmanager/src/main/java/cms/controller/AuthController.java
@@ -19,7 +19,7 @@ public class AuthController {
   
   @RequestMapping(value="login", method=RequestMethod.GET)
   public String loginform() {
-    return "login.do";
+    return "login";
   }
   
   @RequestMapping(value="login", method=RequestMethod.POST)
